4363a25056edbeb92b65759e0110bc8c09457e67
Ahmet Yusuf Purçek <ahmetpurcek19@gmail.com>
2025-01-28 22:30:33 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+// session_start(); satırını kaldırın veya yoruma alın
+$host = 'localhost';
+$dbname = 'blog';
+$username = 'root';
+$password = '';
+
+$conn = mysqli_connect($host, $username, $password, $dbname);
+
+if (!$conn) {
+    die("Veritabanı bağlantısı başarısız: " . mysqli_connect_error());
+}
+
+mysqli_set_charset($conn, "utf8mb4");
+?> 
